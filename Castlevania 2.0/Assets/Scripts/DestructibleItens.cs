99d2a77d510956492f1129115b2e13993d275deb
MarioGermoliato <mgermoliato@yahoo.com.br>
2020-04-30 04:23:33 -0300

    Adição de hitbox nos ataques, implementação do sistema de drop de itens destruindo objetos do cenário e início do código de inimigos

---
+++ Castlevania 2.0/Assets/Scripts/DestructibleItens.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleItens : MonoBehaviour
+{
+    public enum ItensDrop {LittleHeart, BigHeart, Upgrade }
+
+    public ItensDrop dropItem;
+
+    [SerializeField]
+    private GameObject powerUp;
+    [SerializeField]
+    private GameObject smallHeart;
+    [SerializeField]
+    private GameObject bigHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DropItem()
+    {
+        if (dropItem == ItensDrop.Upgrade)
+        {
+            if (GlobalStats.powerUps < 2)
+            {
+                Instantiate(powerUp);
+            }
+            else
+            {
+                Instantiate(bigHeart);
+            }
+        }
+        else if (dropItem == ItensDrop.BigHeart)
+        {
+            Instantiate(bigHeart);
+        }
+        else if (dropItem == ItensDrop.LittleHeart)
+        {
+            Instantiate(smallHeart);
+        }
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Whip"))
+        {
+            DropItem();
+        }
+    }
+}
